54f2481bb992ba91614e7b1ad9e1ec81ddbe34c3
Ahmad maulana <amkomarudin7@gmail.com>
2019-09-28 14:08:22 +0700

    OOP DASAR pada PHP#4-Constructor

--- produk.php
+++ produk.php
@@ -2,10 +2,18 @@
 
 class produk
 {
-    public $judul = "judul",
-        $penulis = "penulis",
-        $penerbit = "penerbit",
-        $harga = 0;
+    public $judul,
+        $penulis,
+        $penerbit,
+        $harga;
+
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0)
+    {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
 
     public function getLabel()
     {
@@ -18,18 +26,11 @@ class produk
 // $produk2 = new produk();
 
 
-$produk3 = new produk();
-$produk3->judul = "naruto";
-$produk3->penulis = "naruto";
-$produk3->penerbit = "shonen jump";
-$produk3->harga = "30000";
-
-$produk4 = new produk();
-$produk4->judul = "Uncharted";
-$produk4->penulis = "Neil Druckmann";
-$produk4->penerbit = "sont computer";
-$produk4->harga = "25000";
-
-echo "Film : " . $produk3->getLabel();
+$produk1 = new produk("Naruto", "Masshi Kishimoto", "Shoen Jump", 3000);
+$produk2 = new produk("Uncharted", "Neil Druckmann", "Sony Computer", 25000);
+$produk3 = new produk("dragon Ball");
+echo "komik : " . $produk1->getLabel();
 echo "<br>";
-echo "komik : " . $produk4->getLabel();
+echo "game : " . $produk2->getLabel();
+echo "<br>";
+var_dump($produk3);
